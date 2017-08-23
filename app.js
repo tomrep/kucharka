16c38b8e1c387aa78dc1e068e9c0bc2f410ef401
Repisky <tomas.repisky@accenture.com>
2017-08-23 12:58:27 +0200

    Fix Background

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let recepty = [
         {"id": 1, "name": "Vianočné oriešky", "category": "speci", "subcategory": "vianocne",
-            "time": "-","image": "data/0005.jpg",
+            "time": "-","image": "0005.jpg",
             "description": "Ak vám každý rok medovníky stvrdnú na kameň, tento recept je práve pre vás...",
             "ingredients": ["30dkg hladká múka", "30dkg hrubá múka", "30dkg smetol", "20dkg práškový cukor",
                 "25dkg mleté orechy", "1 vajce"],
@@ -8,24 +8,24 @@ let recepty = [
                 "20dkg masla = vyšľahaného","!VÝJDE 100ks orieškov!"]
         },
         {"id": 2, "name": "Pizza", "category": "slane", "subcategory": "jedlo",
-            "time": "90 minút","image": "data/0002.jpg",
+            "time": "90 minút","image": "0002.jpg",
             "description": "Ako z pravej talianskej reštaurácie.",
             "ingredients": ["260g hladkej múky", "260g polohrubej múky", "4pl olivového oleja",
                 "2kl soli", "1kl cukru", "300ml vody", "42g droždia"],
             "instructions": ["Všetky suroviny zmiešame a necháme hodinu vykysnúť",
                 "Pečieme pri teplote 230°C, teplovzdušný ohrev zo spodu, približne 10 minút"]},
         {"id": 3, "name": "Žltý syrek varený", "category": "speci", "subcategory": "velkonocne",
-             "time": "-","image": "data/0004.jpg",
+             "time": "-","image": "0004.jpg",
              "description": "Bez tejto pochutiny nie je veľká noc veľkou nocou.",
              "ingredients": ["Vajcia", "Syr"],
              "instructions":["VSetko uvar vo vode", "Nechaj vychladnut"]},
         {"id": 4, "name": "Bôčikové rezne", "category": "hlavne", "subcategory": "bravcove",
-            "time": "-","image":"data/0003.jpg",
+            "time": "-","image":"0003.jpg",
             "description": "Rezne, ktoré zmiznú skôr ako stihnete povedať bôčik.",
             "ingredients":["Masko", "Struhanka", "Olej"],
             "instructions":["Masko pomliet a obalit v struhanke", "Oprazit na panvici alebo vo fritaku"]},
         {"id": 5, "name": "Zemiaková polievka", "category": "polievka", "subcategory": "-",
-            "time": "-","image": "data/0001.jpg",
+            "time": "-","image": "0001.jpg",
             "description": "Lahodná polievka plná chuti a zemiakov.",
             "ingredients":["Zemiaky", "olej", "múka"],
             "instructions":["Uvar zemiaky","Urob zaprazku","Zalej vodou","Pridaj zemiaky",
@@ -37,7 +37,7 @@ $(document).ready(function(){
         list.append(`<section class="food-card">
 		<div class="panel-body">
 		  <div class="photo">
-			<img src="${recept.image}" alt="${recept.name}">
+			<img src="data/${recept.image}" alt="${recept.name}">
 		  </div>
 		  <table class="food-info">
 			<tbody><tr>
@@ -55,3 +55,31 @@ $(document).ready(function(){
 	  </section>`);
     }, this);
 });
+
+function filter (type){
+    let list = $('.food-items');
+    list.html(`<h1>${type}</h1>`);
+    recepty.forEach(function(recept) {
+        if(recept.category === 'polievka'){
+            list.append(`<section class="food-card">
+            <div class="panel-body">
+              <div class="photo">
+                <img src="data/${recept.image}" alt="${recept.name}">
+              </div>
+              <table class="food-info">
+                <tbody><tr>
+                  <td>
+                    <h3><a href="#">${recept.name}</a></h3>
+                    <p class="time">Čas prípravy: ${recept.time}</p>
+                    <p class="description">${recept.description}</p>
+                  </td>
+                  <td>
+                    <a class="food-button">Pozri recept</a>
+                  </td>
+                </tr>
+              </tbody></table>
+            </div>
+          </section>`);
+        }
+    });
+}
